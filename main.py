57555d134dc336ded4666f3c35ed622d9486ac6a
gaurav <gparajuli.1997@gmail.com>
2025-09-17 08:19:21 +0200

    added func to save video

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from utils import read_video, save_video
+
 def main():
     print('main is running')
+    input_video_path = r'input_vid/input_video.mp4'
+    frames = read_video(input_video_path)
+
+    # save video
+    save_video(frames, 'output_vid/output.avi')
 
 if __name__ == "__main__":
     main()
--- utils/video.py
+++ utils/video.py
@@ -9,5 +9,13 @@ def read_video(video_path):
             break
         frames.append(frame)
     capture.release()
-    return frame
+    return frames
+
+def save_video(output_video_frames, output_video_path):
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    out = cv2.VideoWriter(output_video_path, fourcc, 24, (output_video_frames[0].shape[1], output_video_frames[0].shape[0]))
+    for frame in output_video_frames:
+        out.write(frame)
+    out.release()
+
 
