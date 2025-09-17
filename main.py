98aeb0e8935ced220581afa09022aabf9b6d96e9
gaurav <gparajuli.1997@gmail.com>
2025-09-17 08:49:11 +0200

    implemented player tracking

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from utils import read_video, save_video
+from trackers import PlayerTracker
 
 def main():
     print('main is running')
     input_video_path = r'input_vid/input_video.mp4'
     frames = read_video(input_video_path)
 
+    # detect players
+    player_tracker = PlayerTracker(model_path='yolov8x')
+    player_detections = player_tracker.detect_frames(frames)
+
+    # draw output
+    ## draw player bounding boxes
+    output_video_frames = player_tracker.draw_bboxes(frames, player_detections)
+
+
     # save video
-    save_video(frames, 'output_vid/output.avi')
+    save_video(output_video_frames, 'output_vid/output.avi')
 
 if __name__ == "__main__":
     main()
