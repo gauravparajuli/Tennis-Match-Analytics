31bbd98cbc6400f95993c10f94171bf0c6f6d546
gaurav <gparajuli.1997@gmail.com>
2025-09-19 05:36:25 +0200

    implemented court keypoint detections

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from utils import read_video, save_video
 from trackers import PlayerTracker, BallTracker
+from court_line_detector import CourtlineDetector
 
 def main():
     print('main is running')
     input_video_path = r'input_vid/input_video.mp4'
     frames = read_video(input_video_path)
 
-    # detect players
+    # detect players and ball
     player_tracker = PlayerTracker(model_path='yolov8x')
     ball_tracker = BallTracker(model_path=r'weights\ball_detection\best.pt')
     player_detections = player_tracker.detect_frames(frames, read_from_stub=True, stub_path='tracker_stubs/player_detections.pkl')
     ball_detections = ball_tracker.detect_frames(frames, read_from_stub=True, stub_path='tracker_stubs/ball_detections.pkl')
 
+    # detect court lines
+    court_model_path = r'weights\tennis_court_keypoints.pth'
+    court_line_detector = CourtlineDetector(court_model_path)
+    court_keypoints = court_line_detector.predict(frames[0])
+
     # draw output
-    ## draw player bounding boxes
+    ## draw player and ball bounding boxes
     output_video_frames = player_tracker.draw_bboxes(frames, player_detections)
     output_video_frames = ball_tracker.draw_bboxes(frames, ball_detections)
+
+    ## draw court keypoints
+    output_video_frames = court_line_detector.draw_keypoints_on_video(output_video_frames, court_keypoints)
 
     # save video
     save_video(output_video_frames, 'output_vid/output.avi')
